7ba8e224dcb12d4d14de003fab21d6d3dddfae48
ShenKing <shen.king.a@gmail.com>
2017-09-24 22:15:58 +0800

    鸽鸽

---
+++ Assets/Scripts/Btn_St.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Btn_St : MonoBehaviour {
+    public void Entergame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
